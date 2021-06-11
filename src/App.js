668bf80331d36c2b84eb87ec895e622cd5383062
böris <boris.yarmoulnik@gmail.com>
2021-06-11 20:58:47 +0300

    feat: finish html-markup and style

--- src/App.js
+++ src/App.js
@@ -1,31 +1,222 @@
+import Header from './components/Header';
+
 function App() {
   return (
     <>
-      <div className="container_wrapper">
-        <header>
-          <a href="/"><img src="./images/logo.png" alt="" /></a>
-        </header>
-        <main>
-          <aside>
-            LEFT FILTER
+      <div className="container__wrapper">
+        <Header />
+
+        <main className="container">
+          <aside className="sidebar__left">
+            <div className="filter_countTransfers block-background">
+              <h3>Количество пересадок</h3>
+              <form>
+                <label>
+                  <input type="checkbox" className="countTransfers_checkbox visually-hidden" />
+                  <span className="checker" />
+                  <span className="countTransfers_name">
+                    Все
+                  </span>
+                </label>
+                <label>
+                  <input type="checkbox" className="countTransfers_checkbox visually-hidden" />
+                  <span className="checker" />
+                  <span className="countTransfers_name">
+                    Без пересадок
+                  </span>
+                </label>
+                <label>
+                  <input type="checkbox" className="countTransfers_checkbox visually-hidden" />
+                  <span className="checker" />
+                  <span className="countTransfers_name">
+                    1 пересадка
+                  </span>
+                </label>
+                <label>
+                  <input type="checkbox" className="countTransfers_checkbox visually-hidden" />
+                  <span className="checker" />
+                  <span className="countTransfers_name">
+                    2 пересадки
+                  </span>
+                </label>
+                <label>
+                  <input type="checkbox" className="countTransfers_checkbox visually-hidden" />
+                  <span className="checker" />
+                  <span className="countTransfers_name">
+                    3 пересадки
+                  </span>
+                </label>
+              </form>
+            </div>
           </aside>
-          <section>
-            <div>TOP FILTERS</div>
 
-            <div>
-              TICKET
-            </div>
-            <div>
-              TICKET
-            </div>
-            <div>
-              TICKET
-            </div>
-            <div>
-              TICKET
+          <section className="container__main">
+            <div className="switcher__route">
+              <button className="button switcher__route_item switcher__route_item-active">
+                Самый дешевый
+              </button>
+              <button className="button switcher__route_item">
+                Самый быстрый
+              </button>
+              <button className="button switcher__route_item">
+                Оптимальный
+              </button>
             </div>
 
-            <button>MORE TICKET</button>
+            <section className="container__ticket">
+              <div className="ticket__item block-background">
+                <div className="ticket__item_header">
+                  <span className="ticket__item_price">
+                    13 400 ₽
+                  </span>
+                  <img src="./images/s7.png" alt="" class="ticket__item_logo" />
+                </div>
+                <div className="ticket__item_info">
+                  <div className="ticket__item_info-city">
+                    <strong>MOW – HKT</strong>
+                    10:45 – 08:00
+                  </div>
+                  <div className="ticket__item_info-time">
+                    <strong>В пути</strong>
+                    21ч 15м
+                  </div>
+                  <div className="ticket__item_info-transfer">
+                    <strong>2 пересадки</strong>
+                    HKG, JNB
+                  </div>
+                </div>
+                <div className="ticket__item_info">
+                  <div className="ticket__item_info-city">
+                    <strong>MOW – HKT</strong>
+                    10:45 – 08:00
+                  </div>
+                  <div className="ticket__item_info-time">
+                    <strong>В пути</strong>
+                    21ч 15м
+                  </div>
+                  <div className="ticket__item_info-transfer">
+                    <strong>2 пересадки</strong>
+                    HKG, JNB
+                  </div>
+                </div>
+              </div>
+
+              <div className="ticket__item block-background">
+                <div className="ticket__item_header">
+                  <span className="ticket__item_price">
+                    13 400 ₽
+                  </span>
+                  <img src="./images/s7.png" alt="" class="ticket__item_logo" />
+                </div>
+                <div className="ticket__item_info">
+                  <div className="ticket__item_info-city">
+                    <strong>MOW – HKT</strong>
+                    10:45 – 08:00
+                  </div>
+                  <div className="ticket__item_info-time">
+                    <strong>В пути</strong>
+                    21ч 15м
+                  </div>
+                  <div className="ticket__item_info-transfer">
+                    <strong>2 пересадки</strong>
+                    HKG, JNB
+                  </div>
+                </div>
+                <div className="ticket__item_info">
+                  <div className="ticket__item_info-city">
+                    <strong>MOW – HKT</strong>
+                    10:45 – 08:00
+                  </div>
+                  <div className="ticket__item_info-time">
+                    <strong>В пути</strong>
+                    21ч 15м
+                  </div>
+                  <div className="ticket__item_info-transfer">
+                    <strong>2 пересадки</strong>
+                    HKG, JNB
+                  </div>
+                </div>
+              </div>
+
+              <div className="ticket__item block-background">
+                <div className="ticket__item_header">
+                  <span className="ticket__item_price">
+                    13 400 ₽
+                  </span>
+                  <img src="./images/s7.png" alt="" class="ticket__item_logo" />
+                </div>
+                <div className="ticket__item_info">
+                  <div className="ticket__item_info-city">
+                    <strong>MOW – HKT</strong>
+                    10:45 – 08:00
+                  </div>
+                  <div className="ticket__item_info-time">
+                    <strong>В пути</strong>
+                    21ч 15м
+                  </div>
+                  <div className="ticket__item_info-transfer">
+                    <strong>2 пересадки</strong>
+                    HKG, JNB
+                  </div>
+                </div>
+                <div className="ticket__item_info">
+                  <div className="ticket__item_info-city">
+                    <strong>MOW – HKT</strong>
+                    10:45 – 08:00
+                  </div>
+                  <div className="ticket__item_info-time">
+                    <strong>В пути</strong>
+                    21ч 15м
+                  </div>
+                  <div className="ticket__item_info-transfer">
+                    <strong>2 пересадки</strong>
+                    HKG, JNB
+                  </div>
+                </div>
+              </div>
+
+              <div className="ticket__item block-background">
+                <div className="ticket__item_header">
+                  <span className="ticket__item_price">
+                    13 400 ₽
+                  </span>
+                  <img src="./images/s7.png" alt="" class="ticket__item_logo" />
+                </div>
+                <div className="ticket__item_info">
+                  <div className="ticket__item_info-city">
+                    <strong>MOW – HKT</strong>
+                    10:45 – 08:00
+                  </div>
+                  <div className="ticket__item_info-time">
+                    <strong>В пути</strong>
+                    21ч 15м
+                  </div>
+                  <div className="ticket__item_info-transfer">
+                    <strong>2 пересадки</strong>
+                    HKG, JNB
+                  </div>
+                </div>
+                <div className="ticket__item_info">
+                  <div className="ticket__item_info-city">
+                    <strong>MOW – HKT</strong>
+                    10:45 – 08:00
+                  </div>
+                  <div className="ticket__item_info-time">
+                    <strong>В пути</strong>
+                    21ч 15м
+                  </div>
+                  <div className="ticket__item_info-transfer">
+                    <strong>2 пересадки</strong>
+                    HKG, JNB
+                  </div>
+                </div>
+              </div>
+
+            </section>
+
+            <button class="button button__showmoreticket block-background">
+              Показать еще 5 билетов!
+            </button>
           </section>
         </main>
       </div>
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="container__header">
+      <a href="/"><img src="./images/logo.svg" alt="" /></a>
+    </header>
+  )
+}
+
+export default Header;
